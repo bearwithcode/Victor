e63fea7a3997da1106d3b86157e82dff81cf438f
bearwithcode <bearbearinmel@gmail.com>
2018-01-12 23:07:17 +0800

    remove comments

--- main.js
+++ main.js
@@ -5,10 +5,6 @@ var app = angular.module('myApp');
  *<2>after route remove github,$log,$anchorScroll
  */
 app.controller('MainController',function ($scope,$interval,$location) {
-    /*var baseUrl = "https://api.github.com/";
-    var subUrl_1 = "users/";*/
-
-    //$scope.toggleErrorMessage = false;
     $scope.countdown = 10;
     $scope.searchedUserName = "angular";
 
@@ -30,9 +26,6 @@ app.controller('MainController',function ($scope,$interval,$location) {
     $scope.userProfile = "User Profile";
 
     $scope.search = function (searchedUserName) {
-        //$log.info("search for " + searchedUserName);
-        //$http.get(baseUrl + subUrl_1 + searchedUserName).then(onUserComplete,onError);
-        //github.getUser(searchedUserName).then(onUserComplete,onError);
         if(countdownInterval){
             $interval.cancel(countdownInterval);
             $scope.countdown = null;
